c239be463f779a321e5da90ebb25158359ac806e
JGH Inled Group <github@inled.es>
2025-05-31 15:54:06 +0200

    Nuevo sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://inled-group.github.io',
-    base: '.',
-
+  site: 'https://inled-group.github.io',
+  base: '.',
 });
